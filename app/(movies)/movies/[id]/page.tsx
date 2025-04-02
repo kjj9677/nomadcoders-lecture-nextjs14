dbb4272a5c525885be3819cb892ee7fa1a0d78b5
Ju <kjj9677@naver.com>
2025-04-02 13:03:59 +0900

    Add initial project structure with pages, components, and styles; implement movie fetching and navigation

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,31 @@
-const MovieDetail = (props) => {
-  return <div>It is Movie Detail</div>;
-};
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
-export default MovieDetail;
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return {
+    title: `Movie ${id}`,
+    description: "Movie detail page",
+  };
+}
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
+}
--- app/(movies)/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-
-export default function Landing() {
-  const path = usePathname();
-  return <h1>path: {path} 입니다.</h1>;
-}
